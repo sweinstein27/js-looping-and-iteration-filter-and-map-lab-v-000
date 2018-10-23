dfea1ed071ebd8639c6e164303c463936dd7406f
Scott Weinstein <sweinstein27@gmail.com>
2018-10-23 15:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ function driverNamesWithRevenueOver(drivers, revenue) {
     });
   }
 
-function exactMatch(drivers, attribute){
+function exactMatch(drivers, matcher){
   return drivers.filter( function (driver){
-    return driver.attribute === attribute;
-  });
+    let matches = false;
+
+  for (const key in matcher) {
+    matches = driver[key] === matcher[key];
+  }
+
+  return matches;
+});
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function(driver){
+      return driver.name
+    })
 }
